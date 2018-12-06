30ca072000310997100e09d27b0ceb3425a06e91
juliataitz <juliataitz@gmail.com>
2018-12-06 18:22:38 -0500

    aws

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,29 @@ var path = require('path');
 
 // Connect string to MySQL
 var mysql = require('mysql');
+
+app.engine('html', require('ejs').renderFile);
+app.locals.hostname = dbconfig.db['cis550.czrs05s7rxjm.us-east-2.rds.amazonaws.com'];
+app.locals.username = dbconfig.db['cis550nyctaxi'];
+app.locals.password = dbconfig.db['jjor2018'];
+app.locals.port = dbconfig.db['3306'];
+app.locals.database = dbconfig.db['CIS550'];
+app.locals.connectionerror = 'successful';
+app.locals.databases = '';
+// var connection = mysql.createConnection({
+//   host     : 'cis550.czrs05s7rxjm.us-east-2.rds.amazonaws.com',
+//   user     : 'cis550nyctaxi',
+//   password : 'jjor2018',
+//   port     : '3306',
+//   database : 'CIS550'
+// });
+
 var connection = mysql.createConnection({
-  host     : 'fling.seas.upenn.edu',
-  user     : 'jtaitz',
-  password : '',
-  database : 'jtaitz'
+    host: dbconfig.db['cis550.czrs05s7rxjm.us-east-2.rds.amazonaws.com'],
+    user: dbconfig.db['cis550nyctaxi'],
+    password: dbconfig.db['jjor2018'],
+    port: dbconfig.db['3306'],
+    database: dbconfig.db['CIS550']
 });
 
 /* GET home page. */
